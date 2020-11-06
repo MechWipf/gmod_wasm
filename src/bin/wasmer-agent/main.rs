d6d1ff6991024ce1964a1327038b129ac6fde6ce
MechWipf <MechWipf@googlemail.com>
2020-11-06 22:49:08 +0100

    throw out tarpc and add fiber_rpc

--- src/bin/wasmer-agent/main.rs
+++ src/bin/wasmer-agent/main.rs
@@ -1,15 +1,54 @@
-use futures::{future::ready, stream, Stream};
-use tarpc::{server, transport};
-use wasm_service::WasmServer;
+use fibers::{Executor, Spawn, ThreadPoolExecutor};
+use fibers_rpc::{
+    client::ClientService, client::ClientServiceBuilder, server::HandleCall, server::Reply,
+    server::ServerBuilder, Call, ProcedureId,
+};
+use futures::Future;
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 
 mod error;
 mod global_state;
 mod wasm;
 mod wasm_service;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //todo implement server
+fn main() {
+    let mut executor = ThreadPoolExecutor::new().unwrap();
 
-    Ok(())
+    let srv_addr = "[::1]:15081".parse().unwrap();
+
+    let mut builder = ServerBuilder::new(srv_addr);
+    builder.add_call_handler(wasm_service::WasmHandler);
+    let server = builder.finish(executor.handle());
+
+    let mon = executor.spawn_monitor(server);
+    let whatever = executor.run_fiber(mon);
+
+    println!("{:?}", whatever);
+}
+
+mod test {
+    use crate::wasm_service::HelloRpc;
+    use fibers::Executor;
+    use fibers::Spawn;
+    use fibers::ThreadPoolExecutor;
+    use fibers_rpc::client::CallClient;
+    use fibers_rpc::client::ClientServiceBuilder;
+    use fibers_rpc::Call;
+    use fibers_rpc::Cast;
+
+    #[test]
+    fn does_this_run() {
+        let mut executor = ThreadPoolExecutor::new().unwrap();
+        let srv_addr = "[::1]:15081".parse().unwrap();
+
+        let service = ClientServiceBuilder::new().finish(executor.handle());
+        let service_handle = service.handle();
+
+        let request = Vec::from(&b"Hello"[..]);
+        let response = HelloRpc::client(&service_handle).call(srv_addr, request);
+        let response = executor.spawn_monitor(response);
+        let response = executor.run_future(response);
+
+        println!("{:?}", response);
+    }
 }
--- src/bin/wasmer-agent/wasm_service.rs
+++ src/bin/wasmer-agent/wasm_service.rs
@@ -1,25 +1,28 @@
-use futures::{future::ready, future::Ready};
-use std::borrow::Cow;
-use tarpc::context;
+use bytecodec::bytes::{BytesEncoder, RemainingBytesDecoder};
+use fibers_rpc::{
+    server::{HandleCall, Reply},
+    Call, ProcedureId,
+};
 
-#[tarpc::service]
-trait WasmService {
-    async fn from_wat(wat: String) -> String;
-    async fn from_binary(module: Cow<'static, [u8]>) -> String;
+pub(crate) struct HelloRpc;
+
+impl Call for HelloRpc {
+    const ID: ProcedureId = ProcedureId(0);
+    const NAME: &'static str = "wasm";
+
+    type Req = Vec<u8>;
+    type ReqEncoder = BytesEncoder<Vec<u8>>;
+    type ReqDecoder = RemainingBytesDecoder;
+
+    type Res = Vec<u8>;
+    type ResEncoder = BytesEncoder<Vec<u8>>;
+    type ResDecoder = RemainingBytesDecoder;
 }
 
-#[derive(Clone)]
-pub struct WasmServer;
+pub(crate) struct WasmHandler;
 
-impl WasmService for WasmServer {
-    type FromWatFut = Ready<String>;
-    type FromBinaryFut = Ready<String>;
-
-    fn from_wat(self, _: context::Context, _wat: String) -> Self::FromWatFut {
-        ready("Cake".to_string())
-    }
-
-    fn from_binary(self, _: context::Context, _module: Cow<'static, [u8]>) -> Self::FromBinaryFut {
-        ready("Cake".to_string())
+impl HandleCall<HelloRpc> for WasmHandler {
+    fn handle_call(&self, request: <HelloRpc as Call>::Req) -> Reply<HelloRpc> {
+        Reply::done(request)
     }
 }
